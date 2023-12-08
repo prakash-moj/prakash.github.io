a30ab325dd03729abac1e77a58ff01f28b3f7d22
Prakash Subramani <prakash.subramani@digital.justice.gov.uk>
2023-12-08 14:15:47 +0000

    TF static analysis

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,27 @@
+terraform {
+  backend "s3" {
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+provider "aws" {
+  alias  = "london"
+  region = "eu-west-2"
+  default_tags {
+    tags = {
+      GithubTeam = "platform"
+    }
+  }
+}
+
+provider "aws" {
+  alias  = "ireland"
+  region = "eu-west-1"
+}
+
 resource "random_string" "random" {
   length           = 16
   special          = true
